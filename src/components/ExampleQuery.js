e5e0cc91986bc992afbd3a264667ea15ffccc817
ishanvepa <ishanvepa171@gmail.com>
2025-05-08 19:16:27 -0400

    some styling hover effects on search bar and sample query

--- src/components/ExampleQuery.js
+++ src/components/ExampleQuery.js
@@ -1,7 +1,7 @@
 export default function ExampleQuery({text}) {
   return (
     <div className="mt-4 ">
-      <p className="inline-block text-sm bg-white/30 text-white px-4 py-1 rounded-full">
+      <p className="inline-block text-sm bg-white/30 text-white transition hover:bg-white/40 px-4 py-1 rounded-full">
         {text}
       </p>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,7 @@ export default function SearchBar() {
         placeholder="Enter your academic search query..."
         className="bg-white px-4 py-2 rounded-l-full w-full max-w-lg focus:outline-none placeholder:text-gray-500"
       />
-      <button className="bg-orange-700 hover:bg-orange-800 text-white px-5 py-2 rounded-r-full">
+      <button className="bg-amber-800 transition hover:bg-amber-900 text-white px-5 py-2 rounded-r-full">
         Search
       </button>
     </div>
