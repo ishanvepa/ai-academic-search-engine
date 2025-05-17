8e5978b01f0ef85a5d163e58ad0c0c9cc45d12dc
ishanvepa <ishanvepa171@gmail.com>
2025-05-17 19:52:38 -0400

    implemented cosine similarity instead of l2

--- backend/rag.py
+++ backend/rag.py
@@ -5,6 +5,7 @@ from langchain_community.document_loaders import JSONLoader
 from langchain_core.documents import Document
 from langchain_community.vectorstores import FAISS
 
+
 #load embeddings model
 model = SentenceTransformerEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={"trust_remote_code":True})
 
@@ -39,10 +40,9 @@ def ingest(papers):
     
     #instantiate vector store
     if vector_store is None:
-        vector_store = FAISS.from_documents(new_docs, model)  # Initialize the vector store
+        vector_store = FAISS.from_documents(new_docs, model, normalize_L2=True)  # Initialize the vector store
     else: 
         vector_store.add_documents(new_docs)
-
     vector_store.save_local(VECTORSTORE_DIR)
 
     return {"success": f"{len(new_docs)} Research Papers successfully ingested"}, 200
@@ -61,6 +61,6 @@ def similarity_search(query, k=5):
     results_dict = {}
     for i, (res, score) in enumerate(results):
         results_dict[str(i)] = res.metadata
-        results_dict[str(i)]["score"] = float(score)
+        results_dict[str(i)]["score"] = float(1 - score)
 
     return results_dict, 200
